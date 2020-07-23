2256f7db61354c95924a3f790327997a5b273508
msfr21 <50188096+msfr21@users.noreply.github.com>
2020-07-23 11:33:06 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,1 @@
-echo "<h1> Test Bash dari Github </h1> <h2>Za WARUDOOOO !!!</h2>" > index.html
+echo "<h1> Test Bash dari Github </h1> <h2>Za WARUDOOOO !!!</h2> <h3>STOPPPEEE TIME</h3>" > index.html
